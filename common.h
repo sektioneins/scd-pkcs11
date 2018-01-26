d55a0f3721b3007bb419b8d04d3f428198954dba
Ben Fuhrmannek <ben@sektioneins.de>
2018-01-26 12:32:08 +0100

    debug output to stderr

--- common.h
+++ common.h
@@ -2,10 +2,13 @@
 // #define _GNU_SOURCE
 #define _XOPEN_SOURCE 700
 
-// the prefix SEC_ is an acronym for SektionEins Cryptoki 
+// the prefix SEC_ is an acronym for SektionEins Cryptoki
 
 // #define SEC_DEBUG
+// #define SEC_LOG_STDERR
+#ifndef SEC_LOG
 #define SEC_LOG "/tmp/ck_log.txt"
+#endif
 
 #define SEC_MAJOR_VERSION 0
 #define SEC_MINOR_VERSION 0
@@ -23,8 +26,14 @@ typedef unsigned int uint;
 /* ----- DEBUGGING + LOGGING */
 
 #ifdef SEC_DEBUG
-#define SDEBUG(msg...) \
-    {FILE *f;f=fopen(SEC_LOG, "a+");if(f){fprintf(f,"%s:%u %s #> ",__FILE__, __LINE__, __func__);fprintf(f, msg);fprintf(f,"\n");fclose(f);}}
+
+#define SDEBUG_PRINT(f, msg...) \
+	{fprintf(f,"%s:%u %s #> ",__FILE__, __LINE__, __func__);fprintf(f, msg);fprintf(f,"\n");}
+#ifdef SEC_LOG_STDERR
+#define SDEBUG(msg...) {SDEBUG_PRINT(stderr, msg);}
+#else
+#define SDEBUG(msg...) {FILE *f;f=fopen(SEC_LOG, "a+");if(f){SDEBUG_PRINT(f, msg);fclose(f);}}
+#endif
 #define sec_log(msg...) SDEBUG(msg)
 #define sec_log_err(msg...) SDEBUG(msg)
 
@@ -37,4 +46,4 @@ typedef unsigned int uint;
 #define sec_log_err(msg...) \
 	fprintf(stderr, msg)
 
-#endif    
+#endif
